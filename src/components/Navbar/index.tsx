8972a20ad4e3b4c83523eb7bdf2f8277f6745298
Vinicius Montes <viniciusmontes@outlook.com.br>
2022-12-13 23:16:40 -0300

    Componente Navbar e estilizacao da fonte

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,23 @@
+const Navbar = () => {
+  return (
+    <nav className="bg-primary">
+      <div>
+        <a href="link">
+          <h4>Carros Top</h4>
+        </a>
+        <div>
+          <ul>
+            <li>
+              <a href="link">HOME</a>
+            </li>
+            <li>
+              <a href="link">CATÁLOGO</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
